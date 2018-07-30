ee885c949abbeee7180ebcf7d8f4d0d502021cb2
Gregor Cox <gregorcox@gmail.com>
2018-07-30 15:25:57 +0100

    predicts episode 2

--- whoDunnit.js
+++ whoDunnit.js
@@ -1,15 +1,32 @@
 // #### Episode 1
 
-const scenario = {
-  murderer: 'Miss Scarlet',
-  room: 'Library',
-  weapon: 'Rope'
-};
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+//"The murderer is Miss Scarlet"
 
-const declareMurderer = function() {
-  return `The murderer is ${scenario.murderer}.`;
+// #### Episode 2
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
 }
 
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
 const verdict = declareMurderer();
 console.log(verdict);
-//"The murderer is Miss Scarlet"
+//"Cannot change a constant variable"
